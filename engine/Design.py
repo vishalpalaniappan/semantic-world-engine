fc07cd0adf45958078d0be525216226fe6bd1d39
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-16 09:04:33 -0500

    Load the actual metadata and save in design object.

--- engine/Design.py
+++ engine/Design.py
@@ -1,12 +1,26 @@
 import json
+from pathlib import Path
 
+'''
+This class represents the design. It accepts a root design JSON
+file and loads the metadata from the relevant files. It then intializes
+the state machine and accepts the inputs from the user while performing
+the necessary world transformations as described by the semantic primitives
+that build the behavior.
+'''
 class Design():
 
-    def __init__(self, desighPath):
-        self.designPath = desighPath
+    def __init__(self, designPath):
+        self.designPath = designPath
         self.loadDesign()
 
     def loadDesign(self):
+        '''
+        Loads the design from the specified file. It loads
+        the meta data from each of the relevant files.
+        
+        :param self: Reference to instantiated object.
+        '''
         with open(self.designPath) as f:
             self.designMeta = json.loads(f.read())
 
@@ -14,9 +28,18 @@ class Design():
             self.loadDesignMeta(file)
         
     def loadDesignMeta(self, key):
+        '''
+        Loads the metadata for the specified key.
+        
+        :param self: Reference to instantiated object.
+        :param key: Key of metadata being loaded.
+        '''
         if key in self.designMeta["files"]:
-            self.state_machine = self.designMeta["files"]["initial_world_state"]
-            print("Loaded design meta:", key)
+            original_path = Path(self.designPath)
+            new_path = original_path.parent / self.designMeta["files"][key]
+            with open(new_path) as f:
+                setattr(self, key, json.loads(f.read()))
+            print("Loaded design meta:", getattr(self, key))
         else:
             raise Exception("Design is missing initial world state specification")
             
